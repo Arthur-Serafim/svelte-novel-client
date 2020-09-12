0f3fb931f321373679c008e1f34b526d80130879
Arthur-Serafim <dev.seraph.arthur@gmail.com>
2020-09-12 10:21:38 -0300

    Add goodbye message. Today is a sad day

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
   import Completed from "./views/Completed.svelte";
   import Category from "./views/Category.svelte";
   import Search from "./views/Search.svelte";
+  import Error from "./views/Error.svelte";
 
   export let url = "";
 </script>
@@ -16,26 +17,38 @@
     padding: 0;
     font-family: Merriweather;
   }
+
+  :global(body::-webkit-scrollbar) {
+    display: none;
+  }
+
+  .blur {
+    filter: blur(8px);
+    -webkit-filter: blur(8px);
+  }
 </style>
 
 <div>
-  <Router {url}>
-    <div>
-      <Route path="/" component={Home} />
-      <Route path="/featured" component={Featured} />
-      <Route path="/completed" component={Completed} />
-      <Route path="/search/:phrase" let:params>
-        <Search phrase={params.phrase} />
-      </Route>
-      <Route path="/category/:category" let:params>
-        <Category category={params.category} />
-      </Route>
-      <Route path="/novel/:name" let:params>
-        <Novel name={params.name} />
-      </Route>
-      <Route path="/:name/:chapter" let:params>
-        <Read name={params.name} chapter={params.chapter} />
-      </Route>
-    </div>
-  </Router>
+  <Error />
+  <div class="blur">
+    <Router {url}>
+      <div>
+        <Route path="/" component={Home} />
+        <Route path="/featured" component={Featured} />
+        <Route path="/completed" component={Completed} />
+        <Route path="/search/:phrase" let:params>
+          <Search phrase={params.phrase} />
+        </Route>
+        <Route path="/category/:category" let:params>
+          <Category category={params.category} />
+        </Route>
+        <Route path="/novel/:name" let:params>
+          <Novel name={params.name} />
+        </Route>
+        <Route path="/:name/:chapter" let:params>
+          <Read name={params.name} chapter={params.chapter} />
+        </Route>
+      </div>
+    </Router>
+  </div>
 </div>
---
+++ src/views/Error.svelte
@@ -0,0 +1,38 @@
+<div class="not-found">
+  <h1>Due to the international copyright &copy; law, the website's source for content is unstable, so it will be closed for an undefined time.</h1>
+  <h1>Thank you for choosing Read Novel. You, reader, is the reason that made us come this far. Take care, and have a wonderful life.</h1>
+</div>
+
+<style>
+  .not-found {
+    width: 100%;
+    height: 100vh;
+    background: #252525;
+    opacity: .9;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+
+
+    align-items: center;
+    justify-content: center;
+    z-index: 1000000;
+    box-sizing: border-box;
+    padding: 0 200px;
+    overflow: hidden;
+    user-select: none;
+  }
+
+  h1 {
+    text-align: center;
+    color: white;
+    font-weight: bold
+  }
+
+  h1:last-child {
+    font-size: 24px;
+    color: #ddd
+  }
+</style>
